65bcedfdeae46ad636b20beb51a38eadc8f04fd4
panutthon <93002896+panutthon@users.noreply.github.com>
2024-08-21 17:13:13 +0700

    Install Preline UI with Next.js using Tailwind CSS

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import Link from "next/link";
 
-const NotFound: React.FC = () => {
-    return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-        </div>
-    );
-};
-
-export default NotFound;
+export default function NotFound() {
+  return (
+    <div>
+        <h1>404</h1>
+        <p>Not Found</p>
+        <Link   href="/"> click to Home</Link>
+    </div>
+  )
+}
